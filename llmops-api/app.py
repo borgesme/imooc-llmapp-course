e6b4e8aa73c52d57b9f4f6a2d1595a42f8e5d442
borgesme <jdzhao.me@gmail.com>
2026-07-02 23:16:12 +0800

    feat: 2-1-3 PyCharm编辑器配置与插件安装

---
+++ llmops-api/app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2026/7/2 23:11
+@Author  : borgesme@gmail.com
+@File    : app.py
+"""
+from flask import Flask
+
+print("hello world!")
+app = Flask()
