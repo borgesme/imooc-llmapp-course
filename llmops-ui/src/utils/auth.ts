4c4364df19f8871d72f88d1b608dac414adf885d
borgesme <jdzhao.me@gmail.com>
2026-07-05 02:48:16 +0800

    3-1-7 项目页面模板与路由配置，实现路由守卫功能

---
+++ llmops-ui/src/utils/auth.ts
@@ -0,0 +1,5 @@
+// 检测是否登录
+export const isLogin = () => {
+  // TODO: 授权认证逻辑待实现
+  return true
+}
---
+++ llmops-ui/src/views/pages/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p>这是主页</p>
+</template>
+
+<style scoped></style>
